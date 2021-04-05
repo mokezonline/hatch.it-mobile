6e348f31ff907bed5bce420131b57b0dcd7cc0a0
Zachary Cush <zachcushonline@gmail.com>
2021-04-05 02:06:34 -0700

    Add users to database on sign up screen submission

--- screens/login.jsx
+++ screens/login.jsx
@@ -1,11 +1,22 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { SafeAreaView, View, Text, StyleSheet, Pressable, TextInput } from 'react-native';
+import axios from 'axios';
 
 const SignUp = () => {
-  const onSubmit = () => {
+  const [ email, setEmail ] = useState('');
+  const [ user, setUser ] = useState('');
+  const [ password, setPassword ] = useState('');
 
+  const handleSubmit = () => {
+    axios.post('http://10.0.2.2:3000/users', { email, user, password })
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
   }
-  
+
   return (
     <SafeAreaView style={styles.wrapper}>
       <Text style={styles.header}>Sign Up</Text>
@@ -13,19 +24,22 @@ const SignUp = () => {
         style={styles.input}
         placeholder="Email"
         placeholderTextColor="#a4f0c4"
+        onChangeText={setEmail}
       />
       <TextInput
         style={styles.input}
         placeholder="Name"
         placeholderTextColor="#a4f0c4"
+        onChangeText={setUser}
       />
       <TextInput
         style={styles.input}
         placeholder="Password"
         placeholderTextColor="#a4f0c4"
         secureTextEntry={true}
+        onChangeText={setPassword}
       />
-      <Pressable style={({ pressed }) => [
+      <Pressable onPress={handleSubmit} style={({ pressed }) => [
         { backgroundColor: pressed ? "#c4e5eb" : "#a4f0c4" },
         styles.submit
         ]}>
