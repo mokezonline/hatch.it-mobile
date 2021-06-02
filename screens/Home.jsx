7f8098c8ddb2869560deb6604b89b138fc3f69bd
Zachary Cush <zachcushonline@gmail.com>
2021-06-02 10:39:59 -0700

    Implement home screen skeletion

---
+++ screens/Home.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+const Home = () => {
+  const [ pet, updatePet ] = useState('');
+  const [todos, updateTodos] = useState([]);
+
+  
+};
+
+export default Home;
